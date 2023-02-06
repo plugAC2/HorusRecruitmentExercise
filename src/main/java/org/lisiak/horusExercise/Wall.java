64163c89d8536c90c45faf034b9317f71ee5b636
plugAC2 <mts.lisiak@gmail.com>
2023-02-06 19:08:54 +0100

    finishing

--- src/main/java/org/lisiak/horusExercise/Wall.java
+++ src/main/java/org/lisiak/horusExercise/Wall.java
@@ -5,6 +5,8 @@ import org.lisiak.horusExercise.interfaces.Structure;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Wall implements Structure {
     private List<Block> blocks;
@@ -13,21 +15,41 @@ public class Wall implements Structure {
         this.blocks = blocks;
     }
 
+    //znajduje pierwszy blok o poszukiwanym kolorze lub wyrzuca pustego optionala
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        return Optional.empty();
+        if (color == null) {
+            throw new IllegalArgumentException("Color is null...");
+        }
+        List<Block> allMatchingBlocks = findAllByPredicate(block -> block.getColor().equals(color));
+        return allMatchingBlocks.stream()
+                .findFirst();
     }
 
+    //znajduje listę bloków o wymaganym materiale
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return null;
+        if (material == null) {
+            throw new IllegalArgumentException("Material is null...");
+        }
+        return findAllByPredicate(block -> block.getMaterial().equals(material));
     }
 
+    //zlicza bloki w liście
     @Override
     public int count() {
         return blocks.stream()
                 .mapToInt(Block::count)
                 .sum();
+    }
+
+    //znajduje bloki na podstawie podanego predykatu (lambdy)
+    private List<Block> findAllByPredicate(Predicate<Block> predicate) {
+        return blocks.stream()
+                .map(block -> block.getOptionalOfMatchingBlock(predicate))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
     }
 
     public void setBlocks(List<Block> blocks) {
--- src/main/java/org/lisiak/horusExercise/interfaces/Block.java
+++ src/main/java/org/lisiak/horusExercise/interfaces/Block.java
@@ -1,5 +1,8 @@
 package org.lisiak.horusExercise.interfaces;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 public interface Block {
     String getColor();
     String getMaterial();
@@ -8,5 +11,13 @@ public interface Block {
     default int count() {
         return 1;
     }
+
+    default Optional<Block> getOptionalOfMatchingBlock(Predicate<Block> predicate) {
+        if (predicate.test(this)) {
+            return Optional.of(this);
+        }
+        return Optional.empty();
+    }
+
 
 }
